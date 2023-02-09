ea9a4366f56a455e13ebc45ed2258814dad855d7
Levex <123969202+levexprotocol@users.noreply.github.com>
2023-02-09 16:08:26 +0600

    Add files via upload

---
+++ verify-script.sh
@@ -0,0 +1,33 @@
+#!bin/bash
+
+echo starting verify contact network $1.
+
+truffle run verify ControllerV1 --network  $1
+truffle run verify CronosDexAggregatorV1 --network $1
+truffle run verify LTimePool --network  $1
+truffle run verify LPoolDepositor --network $1
+truffle run verify LPoolDepositorDelegator --network $1
+truffle run verify OpenLevV1 --network  $1
+truffle run verify OpenLevV1Lib --network  $1
+
+truffle run verify QueryHelper --network $1
+truffle run verify Timelock --network $1
+truffle run verify XOLE --network $1
+truffle run verify ControllerDelegator --network  $1
+truffle run verify DexAggregatorDelegator --network $1
+truffle run verify OpenLevDelegator --network $1
+truffle run verify XOLEDelegator --network $1
+truffle run verify Airdrop --network $1
+
+
+truffle run verify OLEToken --network  $1
+truffle run verify GovernorAlpha --network  $1
+truffle run verify Reserve --network  $1
+truffle run verify TreasuryDelegator --network  $1
+truffle run verify Treasury --network  $1
+truffle run verify OLETokenLock --network  $1
+truffle run verify FarmingPool --network  $1
+
+echo finished verify contact network $1.
+
+
